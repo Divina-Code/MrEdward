c81de9f3f25ea8c691a0906351256cf3070fb730
MrEdward228 <74458189+MrEdward228@users.noreply.github.com>
2020-11-21 17:40:58 +0300

    Update Висилица.py

--- Висилица.py
+++ Висилица.py
@@ -1,8 +1,17 @@
 from random import randint
-while True:
-    word = ("АВСТРАЛИЯ")
-    letter = input ("Введите букву: ")
+word = ("АВСТРАЛИЯ")
+dedant = 10
+game = True
+while game:
+    letter = input ("Введите букву или слово: ")
     if letter in word:
         print ("Есть такая буква!")
     else:
-        print ("Нет такой буквы.")
+        print ("Не подходит.")
+        dedant = dedant - 1
+        print ("Жизни: " + str(dedant))
+        
+    if letter == word:
+        print ("ТЫ ПОБЕДИЛ! Игра окончена")
+        game = False
+    
