f000ec23612c92b0733ddd01501638d5117ea11a
Ekaterina Divina <ekaterinadivina@gmail.com>
2020-11-21 16:25:46 +0300

    Update kill.py

--- kill.py
+++ kill.py
@@ -1,4 +1,8 @@
+from random import randint
+from time import sleep
+
 while True:
-    from random import randint
+    
     a = randint (0,99999999999999999999999999999999999999999999999999999999999999999999999999999999)
     print (a)
+    sleep(1)
