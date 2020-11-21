1bd09d25013c2c44483afa746d571744b3d50aee
MrEdward228 <74458189+MrEdward228@users.noreply.github.com>
2020-11-21 16:55:10 +0300

    Висилица

---
+++ Висилица.py
@@ -0,0 +1,8 @@
+from random import randint
+while True:
+    word = ("АВСТРАЛИЯ")
+    letter = input ("Введите букву: ")
+    if letter in word:
+        print ("Есть такая буква!")
+    else:
+        print ("Нет такой буквы.")
