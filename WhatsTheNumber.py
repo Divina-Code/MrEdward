2fb141aad29b1b05f5a0072ae8073acd4250086d
MrEdward228 <74458189+MrEdward228@users.noreply.github.com>
2020-11-28 16:24:19 +0300

    Create WhatsTheNumber.py

---
+++ WhatsTheNumber.py
@@ -0,0 +1,21 @@
+
+from random import randint
+gameover = False
+computer_number = randint (1,1000000)
+lives = 50
+while gameover != True:
+    print ()
+    a = int(input("What's the number?: "))
+
+    if a == computer_number:
+         print ("YAY")
+         gameover = True
+
+    if a > computer_number:
+        lives = lives - 1
+        print ("Too big. lives: ", lives)
+    else:
+        lives = lives - 1
+        print("Too small. lives: ", lives)
+    if lives == 0:
+        print("You loose! The number was", computer_number)
