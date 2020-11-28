9bbf40ed0ce843e759d6967a4be983c94a15fb22
MrEdward228 <74458189+MrEdward228@users.noreply.github.com>
2020-11-28 16:54:13 +0300

    Update Висилица.py

--- Висилица.py
+++ Висилица.py
@@ -1,23 +1,27 @@
 from random import randint
-words = ["АВСТРАЛИЯ","ИНДИЯ","ДЖУБУТИ","АНГЛИЯ","ФИЛИППИНЫ"]
+words = ["АВСТРАЛИЯ","ИНДИЯ","КИТАЙ","АНГЛИЯ","ГЕРМАНИЯ"]
 random_index = randint (0, 4) 
 word = words [random_index]
 lives = 10
 game = True
+letters = []
 while game:
     print (" "+"__ "*len(word))
     letter = input ("Введите букву или слово: ")
-    if letter in word:
+    if letter == word:
+        print ("ТЫ ПОБЕДИЛ! Игра окончена.")
+        game = False
+    elif letter in word:
         print ("Есть такая буква!")
+        letters.append(letter)
+        print ("Угаданные буквы: ", letters)
     else:
         print ("Не подходит.")
         lives = lives - 1
-        print ("Жизни: " + str(lives))
-        
-    if letter == word:
-        print ("ТЫ ПОБЕДИЛ! Игра окончена")
-        game = False
+        print ("Осталось", lives, "жизней")
+        print ("Угаданные буквы: ", letters)
     if lives == 0:
         print ("Ты проиграл...")
+        print ("Слово было", word)
         game = False
-    
+
