a5fd2d547f4e62d1aa4f0a023d4055e37e6d2d10
MrEdward228 <74458189+MrEdward228@users.noreply.github.com>
2020-11-28 17:33:15 +0300

    Update Висилица.py

--- Висилица.py
+++ Висилица.py
@@ -1,27 +1,36 @@
 from random import randint
-words = ["АВСТРАЛИЯ","ИНДИЯ","КИТАЙ","АНГЛИЯ","ГЕРМАНИЯ"]
-random_index = randint (0, 4) 
-word = words [random_index]
-lives = 10
-game = True
+words  = ["австралия", "индия", "китай", "англия", "германия"]
+random_index = randint(0, 4)
+word = words[random_index]
+game = True 
+lives = 10 
 letters = []
 while game:
-    print (" "+"__ "*len(word))
-    letter = input ("Введите букву или слово: ")
-    if letter == word:
-        print ("ТЫ ПОБЕДИЛ! Игра окончена.")
-        game = False
-    elif letter in word:
-        print ("Есть такая буква!")
-        letters.append(letter)
-        print ("Угаданные буквы: ", letters)
-    else:
-        print ("Не подходит.")
-        lives = lives - 1
-        print ("Осталось", lives, "жизней")
-        print ("Угаданные буквы: ", letters)
-    if lives == 0:
-        print ("Ты проиграл...")
-        print ("Слово было", word)
-        game = False
+    print(" "+"___ "*len(word))
+    letter = input("Введите букву или слово: ")
 
+    if len(letter)<1:
+        print("Пробел - это пустота")
+    elif len(letter) == 1:
+        if letter in word:
+            if letter not in letters:
+                print("Есть такая буква!")            
+                letters.append(letter)
+            else:
+                print("У тебя дежавю.")
+                lives = lives - 1 
+        else:
+            print("Такой буквы нет.")
+            lives = lives - 1    
+    else:
+        if letter == word:
+            print("ТЫ ПОБЕДИЛ! Игра окончена.")
+        else:
+            print("Не угадал! Игра окончена, ТЫ ПРОИГРАЛ")
+            print("Слово было", word)
+        game = False
+    print("Осталось", lives, "жизней", "Угаданные буквы: ", letters)
+    if lives == 0:
+        print("Жизни закончились, ты проиграл")
+        print("Слово было", word)
+        game = False
