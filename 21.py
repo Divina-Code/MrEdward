0e3b16198c22a76edaf4d1ac4bf2d771df98ea90
MrEdward228 <74458189+MrEdward228@users.noreply.github.com>
2020-12-12 17:50:26 +0300

    Update 21.py

--- 21.py
+++ 21.py
@@ -1,13 +1,30 @@
 from random import randint
 print("добро пожаловать в игру 21")
-eee = int(input("Сколько игроков будет?"))
+eee = int(input("Сколько будет игроков? "))
 print()
 players = []
 for yeet in range(eee):
-    name = input("Введите имя " +str(yeet+1)+"-го игрока:" )
+    name = input("Введите имя " +str(yeet+1)+"-го игрока: " )
     players.append(name)
+print("Игроки: ", players)
 pointss = []
 for points in range(eee):
     random_points = randint(1, 10)
-    print(players[points], "Ваш счёт:", random_points)
+    pointss.append(random_points)
+    print(players[points], "Ваш счёт: ", random_points)
+print("Очки:",pointss)
+game = True
+while game:
+    for a in range(eee):
+        answer = input(players[a]+" Будите брать карту? [Да/Нет] \n")
+        answer = answer.upper()
+        answer = answer.strip()
+        if answer == "ДА":
+            pass
+        elif answer == "НЕТ":
+            pass
+        else:
+            print("А я не понял так да или нет? \n")
+
+
 
